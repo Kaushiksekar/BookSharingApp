8a77176182221bbc110198a7c678c1cb768fb73c
Kaushiksekar <kaushik3993@gmail.com>
2019-11-11 23:07:46 +0530

    Commit for spring boot project setup for book sharing app

---
+++ src/test/java/com/sharing/book/BookSharingApp/BookSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharing.book.BookSharingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
